d53ae7c540188cf774c177fdd6fa3d1e6d7cdf55
visualstudio <visualstudio@230-PC16>
2019-01-31 11:45:19 -0500

    i created a function and loaded it

--- wwwk4-744302/js/main.js
+++ wwwk4-744302/js/main.js
@@ -1,1 +1,10 @@
-﻿// Your code here!
+﻿var date = new Date()
+document.body.innerHTML = "<h1>the DATE TODAY IS " + (date.getMonth() + 1) + "/" + date.getDate() + "/" + date.getFullYear() + "</h1>"
+
+
+
+function myFunction() {
+    alert("hello,welcome to my page");
+    var date = new Date();
+    document.body.innerHTML = "<h1>the DATE TODAY IS " + (date.getMonth() + 1) + "/" + date.getDate() + "/" + date.getFullYear() + "</h1>"
+}
